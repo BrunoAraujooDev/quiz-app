aabeefb4a2451fcd405bd32bc5960465d088d11a
Bruno Araujo <brunoaraujo2395@gmail.com>
2023-04-11 14:57:38 -0300

    criacao do projeto

---
+++ lib/models/question.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Question {
+  final String id;
+  final String category;
+  final String correctAnswer;
+  final List<String> incorrectAnswers;
+  final String question;
+  final List<String> tags;
+  final String difficulty;
+
+  Question({
+    required this.id,
+    required this.category,
+    required this.correctAnswer,
+    required this.incorrectAnswers,
+    required this.question,
+    required this.tags,
+    required this.difficulty,
+  });
+}
