c43c295c039502326e8288366e962197a7abccd9
Bruno Araujo <brunoaraujo2395@gmail.com>
2023-04-12 11:26:56 -0300

    criando prototipo da tela inicial

---
+++ lib/view/landing_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(color: Color.fromRGBO(3, 4, 94, 1)),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'A quiz that challenges your knowlegde!',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            Container(
+              width: 200,
+              margin: const EdgeInsets.only(top: 20),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(255, 195, 0, 1),
+                  elevation: 1,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 20,
+                  ),
+                ),
+                child: const Text(
+                  'Start',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
